2fd51e25e15c5f1a1fffb8242b42ab4178baba9e
Andy Gray <30691631+codingWithAndy@users.noreply.github.com>
2024-06-03 19:13:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,52 @@
 from setuptools import setup, find_packages
 
+# setup(
+#     name='comparative_judgement',
+#     version='0.0.1',
+#     packages=find_packages(),
+#     install_requires=[
+#         'numpy',
+#         'scipy',
+#         'matplotlib',
+#         'ray',
+#     ],
+#     author='Andy Gray',
+#     description='A package for conducting Comparative Judgement',
+#     long_description=open('README.md').read(),
+#     long_description_content_type='text/markdown',
+# )
+
+VERSION = "0.0.1"  # PEP-440
+
+NAME = "comparative_judgement"
+
+INSTALL_REQUIRES = [
+    "pandas",
+    "numpy",
+    "ray",
+    "scipy",
+]
+
+
 setup(
-    name='comparative_judgement',
-    version='0.0.1',
-    packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'scipy',
-        'matplotlib',
-        'ray',
+    name=NAME,
+    version=VERSION,
+    description="A package for conducting Comparative Judgement",
+    url="https://github.com/codingWithAndy/comparative_judgement",
+    project_urls={
+        "Source Code": "https://github.com/codingWithAndy/comparative_judgement",
+    },
+    author="Andy Gray",
+    author_email="",
+    license="",
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
     ],
-    author='Andy Gray',
-    description='A package for conducting Comparative Judgement',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+    # Snowpark requires Python 3.8
+    python_requires=">=3.8",
+    # Requirements
+    install_requires=INSTALL_REQUIRES,
+    packages=["comparative_judgement"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
 )
